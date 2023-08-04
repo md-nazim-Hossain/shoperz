0cc4bc8f9f8fb44f01c908e6a20497dc24985c6b
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2023-08-04 15:50:55 +0600

    fix:husky issues

--- src/components/productsFilter/FilterByPrice.tsx
+++ src/components/productsFilter/FilterByPrice.tsx
@@ -1,41 +1,35 @@
-import React from "react";
-import { Checkbox } from "../ui/checkbox";
 import { Product } from "@/types";
 import { formatPriceFilterKey } from "@/utils/format-data";
+import { Checkbox } from "../ui/checkbox";
 type Props = {
-    product: Map<string, Product[]>;
+  product: Map<string, Product[]>;
 };
 function FilterByPrice({ product }: Props) {
-    const sortProduct = Array.from(product).sort(
-        ([keyA], [keyB]) => +keyA - +keyB
-    );
+  const sortProduct = Array.from(product).sort(
+    ([keyA], [keyB]) => +keyA - +keyB
+  );
 
-    return (
-        <div className="p-5 space-y-5 border rounded-md">
-            <p className="font-medium">Price</p>
-            <div className="space-y-5">
-                {sortProduct.map(([key, value], index: number) => {
-                    return (
-                        <div
-                            key={index}
-                            className="flex items-center space-x-2"
-                        >
-                            <Checkbox onChange={e => console.log(e)} id={key} />
-                            <label
-                                htmlFor={key}
-                                className="flex capitalize items-center gap-x-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                                <h5>{formatPriceFilterKey(key)}</h5>
-                                <h5 className="text-secondary/60">
-                                    ({value?.length})
-                                </h5>
-                            </label>
-                        </div>
-                    );
-                })}
+  return (
+    <div className="p-5 space-y-5 border rounded-md">
+      <p className="font-medium">Price</p>
+      <div className="space-y-5">
+        {sortProduct.map(([key, value], index: number) => {
+          return (
+            <div key={index} className="flex items-center space-x-2">
+              <Checkbox onChange={e => console.log(e)} id={key} />
+              <label
+                htmlFor={key}
+                className="flex capitalize items-center gap-x-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                <h5>{formatPriceFilterKey(key)}</h5>
+                <h5 className="text-secondary/60">({value?.length})</h5>
+              </label>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default FilterByPrice;
--- src/components/shared/SwiperContainer.tsx
+++ src/components/shared/SwiperContainer.tsx
@@ -1,82 +1,82 @@
 "use client";
 
-import React, { useRef } from "react";
+import { useRef } from "react";
 
-import { Navigation } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
 import { Product } from "@/types";
-import PrimaryProductCard from "./PrimaryProductCard";
+import { LiaAngleLeftSolid, LiaAngleRightSolid } from "react-icons/lia";
 import "swiper/css";
 import "swiper/css/navigation";
-import { LiaAngleLeftSolid, LiaAngleRightSolid } from "react-icons/lia";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import PrimaryProductCard from "./PrimaryProductCard";
 import TitleWrapper from "./TitleWrapper";
 
 type Props = {
-    title: string;
-    products: Product[];
+  title: string;
+  products: Product[];
 };
 function SwiperContainer({ title, products }: Props) {
-    const swiperRef: any = useRef<typeof Swiper | any>(null);
+  const swiperRef = useRef<typeof Swiper | any>(null);
 
-    return (
-        <div className="space-y-5 container">
-            <div className="flex justify-between items-center">
-                <TitleWrapper title={title} />
-                <div>
-                    <button
-                        onClick={() => {
-                            if (swiperRef) {
-                                swiperRef?.current.slidePrev();
-                            }
-                        }}
-                        className="text-secondary/70 disabled:text-secondary/20"
-                    >
-                        <LiaAngleLeftSolid size={24} />
-                    </button>
-                    <button
-                        onClick={() => {
-                            if (swiperRef) {
-                                swiperRef?.current?.slideNext();
-                            }
-                        }}
-                        className="text-secondary/70 disabled:text-secondary/20"
-                    >
-                        <LiaAngleRightSolid size={24} />
-                    </button>
-                </div>
-            </div>
-            <Swiper
-                onSwiper={swiper => {
-                    swiperRef.current = swiper;
-                }}
-                modules={[Navigation]}
-                slidesPerView={4}
-                spaceBetween={20}
-                breakpoints={{
-                    100: {
-                        slidesPerView: 1,
-                    },
-                    640: {
-                        slidesPerView: 2,
-                    },
-                    768: {
-                        slidesPerView: 3,
-                    },
-                    1024: {
-                        slidesPerView: 4,
-                    },
-                }}
-            >
-                {products.map((product: Product, index) => {
-                    return (
-                        <SwiperSlide key={index}>
-                            <PrimaryProductCard product={product} />
-                        </SwiperSlide>
-                    );
-                })}
-            </Swiper>
+  return (
+    <div className="space-y-5 container">
+      <div className="flex justify-between items-center">
+        <TitleWrapper title={title} />
+        <div>
+          <button
+            onClick={() => {
+              if (swiperRef) {
+                swiperRef?.current.slidePrev();
+              }
+            }}
+            className="text-secondary/70 disabled:text-secondary/20"
+          >
+            <LiaAngleLeftSolid size={24} />
+          </button>
+          <button
+            onClick={() => {
+              if (swiperRef) {
+                swiperRef?.current?.slideNext();
+              }
+            }}
+            className="text-secondary/70 disabled:text-secondary/20"
+          >
+            <LiaAngleRightSolid size={24} />
+          </button>
         </div>
-    );
+      </div>
+      <Swiper
+        onSwiper={swiper => {
+          swiperRef.current = swiper;
+        }}
+        modules={[Navigation]}
+        slidesPerView={4}
+        spaceBetween={20}
+        breakpoints={{
+          100: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {products.map((product: Product, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <PrimaryProductCard product={product} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
 }
 
 export default SwiperContainer;
