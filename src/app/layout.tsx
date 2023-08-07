aa91ad84e9559b47e015440f29173d1f7a1c6163
md-nazim-Hossain <nazimhossaindpi@gmail.com>
2023-08-08 00:20:14 +0600

    fix:theming system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="overflow-x-hidden">
       <body className={roboto.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <Providers>
             <Toaster />
             <TopBar />
